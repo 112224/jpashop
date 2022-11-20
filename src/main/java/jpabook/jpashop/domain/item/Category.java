33fd421249413780f986ae1e1f39569c9b0ef745
112224 <didgns25@naver.com>
2022-11-20 14:29:36 +0900

    feature : 연관관계 편의 메서드 생성
    연관관계에서의 원자성을 쉽게 관리하기 위한 메서드 설정.

--- src/main/java/jpabook/jpashop/domain/item/Category.java
+++ src/main/java/jpabook/jpashop/domain/item/Category.java
@@ -32,4 +32,10 @@ public class Category {
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
+
+    //연관관계 편의 메서드
+    public void addChildCategory(Category category) {
+        this.child.add(category);
+        category.setParent(this);
+    }
 }
