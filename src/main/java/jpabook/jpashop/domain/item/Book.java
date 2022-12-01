ab90a60e1283e55a4d50e6f11352f3467f14d840
112224 <didgns25@naver.com>
2022-12-01 22:43:24 +0900

    FEAT : Book Entity 생성메서드 개발

--- src/main/java/jpabook/jpashop/domain/item/Book.java
+++ src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
@@ -8,7 +9,22 @@ import javax.persistence.Entity;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Book extends Item {
     private String author;
     private String isbn;
+
+
+    //== 생성메서드==
+    public static Book createBook(String name, int price, int stockQuantity, String author, String isbn) {
+        Book book = new Book();
+
+        book.setName(name);
+        book.setPrice(price);
+        book.setStockQuantity(stockQuantity);
+        book.setAuthor(author);
+        book.setIsbn(isbn);
+
+        return book;
+    }
 }
