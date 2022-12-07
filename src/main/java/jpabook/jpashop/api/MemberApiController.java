679b969996c2a7b7cdcbe819e5939d70980417e7
112224 <didgns25@naver.com>
2022-12-07 13:54:25 +0900

    FEAT : 회원 수정 api 컨트롤러 개발
    RESTful 한 URL 설계, PUT 의 경우 멱등성이 있어서 변경에 대해 주로 아용하는 편.

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -2,11 +2,10 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
@@ -34,6 +33,20 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid UpdateMemberRequest request) {
+
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    // ==DTO==
+    // inner class 로 DTO를 만들어줄 시 응집성이 좋아지고 유지보수성이 좋아짐.
+    // 전체적으로 사용하는 것이라면 별도의 DTO package 로 분리해야 하겠지만 그게 아니라면 inner 로 가져가자.
+
     @Data
     static class CreateMemberResponse {
         private Long id;
@@ -43,12 +56,20 @@ public class MemberApiController {
         }
     }
 
-    // ==DTO==
-    // inner class 로 DTO를 만들어줄 시 응집성이 좋아지고 유지보수성이 좋아짐.
-    // 전체적으로 사용하는 것이라면 별도의 DTO package 로 분리해야 하겠지만 그게 아니라면 inner 로 가져가자.
     @Data
     static class CreateMemberRequest {
         @NotEmpty
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
+    @Data
+    static class UpdateMemberRequest {
         private String name;
     }
 }
