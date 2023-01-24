3966ad666240b9423b54a17c26402f02683f5792
112224 <didgns25@naver.com>
2023-01-24 19:51:31 +0900

    FEAT : 주문조회 V6 개발 JPA 에서 DTO 직접 조회, 플랫 데이터 최적화
    장점은 한 번의 쿼리로 원하는 데이터를 전부 긁어올 수 있지만, 페이징이 불가능하고(Order 기준으로 불가능) DB에서 보내는 데이터에 중복이 있기에 경우에 따라서는 V5 보다 느릴 수 있다. 추가로 애플리케이션 레이어에서 작업량이 많고 유지보수성이 떨어진다.

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -3,25 +3,36 @@ package jpabook.jpashop.repository.order.query;
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.OrderStatus;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@EqualsAndHashCode(of = "orderId")
 public class OrderQueryDto {
 
     private Long orderId;
     private String name;
-    private LocalDateTime dateTime;
+    private LocalDateTime orderDate;
     private OrderStatus orderStatus;
     private Address address;
     private List<OrderItemQueryDto> orderItems;
 
-    public OrderQueryDto(Long id, String name, LocalDateTime dateTime, OrderStatus orderStatus, Address address) {
+    public OrderQueryDto(Long id, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
         this.orderId = id;
         this.name = name;
-        this.dateTime = dateTime;
+        this.orderDate = orderDate;
         this.orderStatus = orderStatus;
         this.address = address;
+    }
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderItems = orderItems;
     }
 }
